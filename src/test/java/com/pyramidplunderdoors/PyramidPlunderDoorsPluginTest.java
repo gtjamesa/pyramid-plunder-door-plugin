ca80dc402a7677e7744c1b062d6e48fc2feaea9e
James Austen <aus.james@gmail.com>
2025-10-22 13:21:19 +0100

    test: add npc hiding tests

--- src/test/java/com/pyramidplunderdoors/PyramidPlunderDoorsPluginTest.java
+++ src/test/java/com/pyramidplunderdoors/PyramidPlunderDoorsPluginTest.java
@@ -3,23 +3,30 @@ package com.pyramidplunderdoors;
 import com.google.inject.Guice;
 import com.google.inject.testing.fieldbinder.Bind;
 import com.google.inject.testing.fieldbinder.BoundFieldModule;
+import com.pyramidplunderdoors.config.RemoveNpc;
 import com.pyramidplunderdoors.data.Door;
 import java.util.HashMap;
 import java.util.Map;
+import net.runelite.api.Actor;
 import net.runelite.api.Client;
+import net.runelite.api.NPC;
+import net.runelite.api.Player;
 import net.runelite.api.Tile;
 import net.runelite.api.TileObject;
 import net.runelite.api.coords.WorldPoint;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.when;
-import org.mockito.Spy;
 import org.mockito.junit.MockitoJUnitRunner;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -33,8 +40,8 @@ public class PyramidPlunderDoorsPluginTest
 	@Bind
 	private PyramidPlunderDoorsConfig config;
 
-	@Spy
-	PyramidPlunderDoorsPlugin plugin;
+	@InjectMocks
+	PyramidPlunderDoorsPlugin plugin = spy(new PyramidPlunderDoorsPlugin());
 
 	@Before
 	public void before()
@@ -92,5 +99,70 @@ public class PyramidPlunderDoorsPluginTest
 		assertNotNull(res);
 		assertEquals(1932, res.getTile().getWorldLocation().getX());
 		assertEquals(4466, res.getTile().getWorldLocation().getY());
+	}
+
+	@Test
+	public void shouldHideNoNpcs()
+	{
+		doReturn(true).when(plugin).isInPyramidPlunder();
+		when(config.removeNpcs()).thenReturn(RemoveNpc.NONE);
+
+		NPC npc = mock(NPC.class);
+
+		boolean res = plugin.shouldDraw(npc, false);
+		assertTrue(res);
+	}
+
+	@Test
+	public void shouldHideAllNpcs()
+	{
+		doReturn(true).when(plugin).isInPyramidPlunder();
+		when(config.removeNpcs()).thenReturn(RemoveNpc.ALL);
+
+		Player player = mock(Player.class);
+		when(player.getName()).thenReturn("LocalPlayer");
+		when(client.getLocalPlayer()).thenReturn(player);
+
+		NPC npc = mock(NPC.class);
+		when(npc.getId()).thenReturn(7661);
+
+		boolean res = plugin.shouldDraw(npc, false);
+		assertFalse(res);
+	}
+
+	@Test
+	public void shouldHideOtherNpcs()
+	{
+		doReturn(true).when(plugin).isInPyramidPlunder();
+		when(config.removeNpcs()).thenReturn(RemoveNpc.OTHER_PLAYERS);
+
+		Player player = mock(Player.class);
+		when(player.getName()).thenReturn("LocalPlayer");
+		when(client.getLocalPlayer()).thenReturn(player);
+
+		// interacting with another player (hide=true)
+		NPC npc1 = mock(NPC.class);
+		when(npc1.getId()).thenReturn(7661);
+		Actor interacting1 = mock(Actor.class);
+		when(interacting1.getName()).thenReturn("OtherPlayer");
+		when(npc1.getInteracting()).thenReturn(interacting1);
+
+		// interacting with null (hide=true)
+		NPC npc2 = mock(NPC.class);
+		when(npc2.getId()).thenReturn(7661);
+		Actor interacting2 = mock(Actor.class);
+		when(interacting2.getName()).thenReturn(null);
+		when(npc2.getInteracting()).thenReturn(interacting2);
+
+		// interacting with local player (hide=false)
+		NPC npc3 = mock(NPC.class);
+		when(npc3.getId()).thenReturn(7661);
+		Actor interacting3 = mock(Actor.class);
+		when(interacting3.getName()).thenReturn("LocalPlayer");
+		when(npc3.getInteracting()).thenReturn(interacting3);
+
+		assertFalse(plugin.shouldDraw(npc1, false));
+		assertFalse(plugin.shouldDraw(npc2, false));
+		assertTrue(plugin.shouldDraw(npc3, false));
 	}
 }
